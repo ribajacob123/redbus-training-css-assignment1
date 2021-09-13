0d22154719a91260693480cf1e11a93547d9aa71
Riba Jacob <riba.jacob@RIBA-JACOB-MAC.local>
2021-09-14 01:57:39 +0530

    JS looping added for checkboxes

--- main.js
+++ main.js
@@ -1,32 +1,53 @@
+window.onload = function () {
+    const element=[
+        {"q":"Bus staff was wearing masks?","imgsrc":"images/tempcheck.jpg","imgid":"","checkboxid":"","checkboxname":"ck"},
+        {"q":"Bus staff was wearing masks?","imgsrc":"images/tempcheck.jpg","imgid":"","checkboxid":"","checkboxname":"ck"},
+        {"q":"Bus staff was wearing masks?","imgsrc":"images/tempcheck.jpg","imgid":"","checkboxid":"","checkboxname":"ck"},
+        {"q":"Bus staff was wearing masks?","imgsrc":"images/tempcheck.jpg","imgid":"","checkboxid":"","checkboxname":"ck"},
+        {"q":"Bus staff was wearing masks?","imgsrc":"images/tempcheck.jpg","imgid":"","checkboxid":"","checkboxname":"ck"},
+        {"q":"Bus staff was wearing masks?","imgsrc":"images/tempcheck.jpg","imgid":"","checkboxid":"","checkboxname":"ck"},
+        {"q":"Bus staff was wearing masks?","imgsrc":"images/tempcheck.jpg","imgid":"","checkboxid":"","checkboxname":"ck"},
+        {"q":"Bus staff was wearing masks?","imgsrc":"images/tempcheck.jpg","imgid":"","checkboxid":"","checkboxname":"ck"},
+        {"q":"Bus staff was wearing masks?","imgsrc":"images/tempcheck.jpg","imgid":"","checkboxid":"","checkboxname":"ck"},
+        {"q":"No safety measures were followed","imgsrc":"images/homesafety.jpg","imgid":"imglast","checkboxid":"cklast","checkboxname":"ck10"},
+    ]
 
-function ckChange(cb){
+    let content = "";
+    var flexContainer = document.getElementById("flex-container");
+    for (let i = 0; i < element.length; i++) {
+        content += '<div class="container"><div class="visual-aid"><img src="'+element[i]["imgsrc"]+'"id = "'+element[i]["imgid"]+'"><label class="checkbox-container"><input name="'+element[i]["checkboxname"]+'"id = "'+element[i]["checkboxid"]+'" type="checkbox" onclick="ckChange(this)"><span class="checkmark"></span></label></div><p class="query">' +
+            element[i]["q"] + '</p></div>'
+        }
+
+    flexContainer.innerHTML = content
+}
+
+
+
+function ckChange(cb) {
     var imglast = document.getElementById("imglast");
     var imgs = document.getElementsByTagName("img");
 
     var last = document.getElementById("cklast");
     var ckName = document.getElementsByName("ck");
 
-    if(cb.name !="ck10"){
+    if (cb.name != "ck10") {
         last.checked = false;
-        for(var i=0;i<ckName.length;i++){
+        for (var i = 0; i < ckName.length; i++) {
             ckName[i].disabled = false;
         }
-        for(var i = 0; i<imgs.length;i++){
+        for (var i = 0; i < imgs.length; i++) {
             imgs[i].style.opacity = 1;
         }
         imglast.style.opacity = 0.2;
-    }
-
-    else{
+    } else {
         last.disabled = false;
-        for(var i=0;i<ckName.length;i++){
+        for (var i = 0; i < ckName.length; i++) {
             ckName[i].checked = false;
         }
-        for(var i = 0; i<imgs.length;i++){
+        for (var i = 0; i < imgs.length; i++) {
             imgs[i].style.opacity = 0.2;
         }
         imglast.style.opacity = 1;
     }
-
-
 }
